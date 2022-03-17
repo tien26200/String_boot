6dbe76ee891365523f473b66ac9479c5c6e57f2c
Tien Do Xuan <intern.dxtien@tma.com.vn>
2022-03-17 16:51:57 +0700

    String boot

---
+++ src/main/java/com/example/Spring_backend/SpringBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.Spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBackendApplication.class, args);
+	}
+
+}
